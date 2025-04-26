1f4b71a6c10daaf366992759cc543588412a5529
Dimple Singh <dimplesingh@Dimples-MacBook-Air.local>
2025-04-25 22:29:00 -0500

    Deployment fix

--- index.js
+++ index.js
@@ -1,48 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const db = require("./db");
-// require("dotenv").config();
-
-// const app = express();
-
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-
-// // Import Routes
-// const authRoutes = require("./routes/authRoutes");
-// const productRoutes = require("./routes/productRoutes");
-// const categoryRoutes = require('./routes/categoryRoutes');
-// const sellerRoutes = require('./routes/sellerRoutes');
-// const cartRoutes = require('./routes/cartRoutes');
-// const favouriteRoutes = require('./routes/favouriteRoutes');
-// const orderRoutes = require('./routes/orderRoutes');
-// const shippingRoutes = require('./routes/shippingRoutes');
-
-
-// app.use("/api/auth", authRoutes); // Route for authentication
-// app.use('/api/products', productRoutes); // Route for products
-// app.use('/api/categories', categoryRoutes);  // Route for category
-// app.use('/api/sellers', sellerRoutes); // Route for seller
-// app.use('/api/cart', cartRoutes); // Route for cart
-// app.use('/api/favourites', favouriteRoutes); // Route for favourites
-// app.use('/api/orders', orderRoutes);  // Route for MyOrders
-// app.use('/api/shipping', shippingRoutes);  // Route for MyOrders
-
-
-// app.use((req, res, next) => {
-//   console.log('Unhandled request:', req.method, req.url);
-//   res.status(404).json({ message: 'Not found' });
-// });
-
-
-// // Start Server
-// const PORT = process.env.PORT || 8081;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
-
 
 const express = require("express");
 const cors = require("cors");
@@ -53,7 +8,7 @@ const app = express();
 
 // ✅ CORS configuration
 app.use(cors({
-  origin: "http://localhost:3001", // your frontend URL
+  origin: ['http://localhost:3001', 'http://localhost:3000','https://dxs8368.uta.cloud/WDM_Team8'], // your frontend URL
   credentials: true               // if you're using cookies, sessions, or auth headers
 }));
 
