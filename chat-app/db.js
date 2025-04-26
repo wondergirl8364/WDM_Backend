e1d01637cd4a552f48b2b84fe46f4dc7bd36882a
Dimple Singh <dimplesingh@Dimples-MacBook-Air.local>
2025-04-26 13:37:24 -0500

    Fixed changes

--- chat-app/db.js
+++ chat-app/db.js
@@ -1,6 +1,6 @@
-// // db.js
-// const mysql = require('mysql2/promise');
-// require('dotenv').config();
+// db.js
+const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
